72584152e455cf5b4a93e3006952206e1221f01e
unknown <kimskiy@mail.ru>
2024-09-04 13:21:07 +0300

    fixed by comments

--- scripts/index.js
+++ scripts/index.js
@@ -5,24 +5,22 @@ const placesList = document.querySelector('.places__list');
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-const cardCreate = card => {
+const createCard = (card, removeCard) => {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImgEl =  cardElement.querySelector('.card__image');
+  cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
   cardImgEl.src = card.link;
   cardImgEl.alt = card.name;
   cardElement.querySelector('.card__title').textContent = card.name;
-  placesList.append(cardElement);
-  removeCard(cardElement);
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
-const removeCard = card => {
-  card.querySelector('.card__delete-button').addEventListener('click', event => {
-   event.target.parentNode.remove();
-  })
+const removeCard = (event) => {
+  event.target.parentNode.remove();
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(card => {
-  cardCreate(card);
+initialCards.forEach((card) => {
+  placesList.append(createCard(card, removeCard));
 });
